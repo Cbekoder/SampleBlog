bb1f20ee83e75b763b0fca9f8300676b5e1a77e3
Cbekoder <cbekoder@gmail.com>
2023-11-08 18:50:29 +0500

    maqola html to'g'rilandi

--- asosiy/views.py
+++ asosiy/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import *
 def home(request):
     return render(request, 'home.html')
 
@@ -7,4 +7,15 @@ def about(request):
     return render(request, 'about.html')
 
 def blog(request):
-    return render(request, 'blog.html')
+    content = {
+        "maqolalar" : Maqola.objects.all(),
+        "yillar" : Maqola.objects.all().values("date").distinct().order_by("date")
+    }
+    return render(request, 'blog.html', content)
+
+def maqola(request, slug):
+
+    content = {
+        "maqola" : Maqola.objects.get(slug = slug)
+    }
+    return render(request, "maqola.html", content)
